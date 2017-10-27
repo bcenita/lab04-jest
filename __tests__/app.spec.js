4eb8e70232c0616912020e1b2a38a608760eb032
Benneth Cenita <bennethcenita@D-173-250-246-146.dhcp4.washington.edu>
2017-10-27 11:10:00 -0700

    completed lab demo

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -4,3 +4,19 @@ import {greet, invertCase} from '../js/app'; //import the functions to test!
 
 /* tests go here */
 
+describe('Basic math', function() {
+    it('should add numbers correctly', function() {
+        //javascript
+        let result = 1 + 1;
+    
+        //assertion
+        expect(result).toEqual(2);
+    });
+});
+
+describe('invertCase() function', function() {
+    it('should invert case for each letter', function() {
+        let test = invertCase('the quick brown fox jumps over the lazy dog');
+        expect(test).toEqual('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG');
+    });
+});
